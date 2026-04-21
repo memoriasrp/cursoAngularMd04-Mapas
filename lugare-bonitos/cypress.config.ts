e084037f53d5c9d941f089854a40326f3c8c7d4e
Silvia Rodriguez <silviamarp@gmail.com>
2026-04-20 22:33:15 -0500

    uso de cypress

---
+++ lugare-bonitos/cypress.config.ts
@@ -0,0 +1,17 @@
+export default {
+  allowCypressEnv: false,
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "angular",
+      bundler: "webpack",
+    },
+    specPattern: "**/*.cy.ts",
+  },
+};
